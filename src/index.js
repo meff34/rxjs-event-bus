8044e0cc4ee2ce7c4b8b255309a8cc8a705d8f4b
Ivan Gorbunov <i.gorbunov@space307.com>
2018-08-01 20:08:05 +0300

    houston we have a problem with Replay Subjects

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// @flow
+
+import { Subject, Observable, ReplaySubject } from 'rxjs'
+import { mergeAll } from 'rxjs/operators';
+
+type EventType = 'trading_signals:add' | 'trading_signals:remove' | 'event'
+
+type Event = {
+  type: EventType,
+  payload: *,
+}
+
+type BusHistorySettings = Map<EventType, number>
+
+export class Bus {
+  _subjectsEmitter: Subject
+  _streams: Map<EventType, Subject> = new Map()
+
+  constructor(historySettings?: BusHistorySettings) {
+    this._subjectsEmitter = (new Subject()).pipe(mergeAll())
+    // this._subjectsEmitter = (new ReplaySubject()).pipe(mergeAll())
+
+    if (historySettings) {
+      this._initReplaySubjects(historySettings)
+    }
+  }
+
+  select(type: EventType): Observable {
+    this._createStreamIfNotExists(type)
+    return this._streams.get(type).asObservable()
+  }
+
+  read() {
+    return this._subjectsEmitter.asObservable();
+  }
+
+  emit(event: Event): void {
+    this._createStreamIfNotExists(event.type)
+
+    this._streams.get(event.type).next(event)
+  }
+
+  _createStreamIfNotExists(type: EventType) {
+    if (!this._streams.has(type)) {
+      this._addNewStream(type, new Subject())
+    }
+  }
+
+  _updateMainStream(type: EventType) {
+    this._subjectsEmitter.next(this._streams.get(type));
+  }
+
+  _initReplaySubjects(historySettings: BusHistorySettings) {
+    for (var [type, value] of historySettings.entries()) {
+      this._addNewStream(type, new ReplaySubject(value))
+    }
+  }
+
+  _addNewStream(type: EventType, stream: Subject | ReplaySubject) {
+    this._streams.set(type, stream)
+    this._updateMainStream(type)
+  }
+}
---
+++ tests/index.test.js
@@ -0,0 +1,141 @@
+import { Bus } from '../src/index'
+
+const getInstance = settings => new Bus(settings)
+
+test('construct', () => {
+  expect(getInstance).not.toThrow()
+})
+
+test('creates stream after select', () => {
+  const bus = getInstance()
+
+  bus.select('trading_signals:add')
+    .subscribe(e => console.log('add', e, '\n'))
+
+  expect(bus._streams.get('trading_signals:add')).not.toBeUndefined()
+})
+
+test('creates stream after emit', () => {
+  const bus = getInstance()
+
+  bus.emit({type: 'trading_signals:add', payload: 1})
+
+  expect(bus._streams.get('trading_signals:add'))
+    .not.toBeUndefined()
+})
+
+test('can not emit throuth \'select(...)\'', () => {
+  const bus = getInstance()
+  const observable = bus.select('event')
+
+  expect(() => observable.emit({type: 'event', payload: 1})).toThrow()
+})
+
+test('can not emit throuth \'read()\'', () => {
+  const bus = getInstance()
+  const observable = bus.read()
+
+  expect(() => observable.emit({type: 'event', payload: 1})).toThrow()
+})
+
+test('receive data on subscribe', done => {
+  const bus = getInstance()
+
+  const event = {type: 'trading_signals:add', payload: 1}
+
+  bus
+    .select('trading_signals:add')
+    .subscribe(e => {
+      expect(e).toEqual(event)
+      done()
+    })
+
+  bus.emit(event)
+})
+
+test('receives data from several streams throuth \'read()\'', () => {
+  const bus = getInstance()
+  const subscriber = jest.fn();
+
+  bus.read()
+    .subscribe(subscriber)
+
+  bus.emit({type: 'trading_signals:add', payload: 1})
+  bus.emit({type: 'trading_signals:remove', payload: 1})
+
+  expect(subscriber).toHaveBeenCalledTimes(2)
+})
+
+test('receives data from several streams throuth \'read()\' after creation of new stream', () => {
+  const bus = getInstance()
+  const firstSubscriber = jest.fn();
+  const secondSubscriber = jest.fn();
+
+  bus.read()
+    .subscribe(firstSubscriber)
+
+  bus.emit({type: 'trading_signals:add', payload: 1})
+
+  bus.read()
+    .subscribe(secondSubscriber)
+
+  bus.emit({type: 'trading_signals:remove', payload: 1})
+
+  expect(firstSubscriber).toHaveBeenCalledTimes(2);
+  expect(secondSubscriber).toHaveBeenCalledTimes(1);
+})
+
+test('historySettings setup streams', () => {
+  const bus = getInstance(new Map([
+    ['trading_signals:remove', 1],
+    ['trading_signals:add', 1000]
+  ]))
+
+  expect(bus._streams.size).toBe(2)
+})
+
+test('with historySettings receives data from past on subscribe to \'read()\'', () => {
+  const bus = getInstance(new Map([
+    ['trading_signals:remove', 2],
+    ['trading_signals:add', 1]
+  ]))
+
+  const subscriber = jest.fn();
+
+  bus
+    .read()
+    .subscribe(subscriber)
+
+  bus.emit({type: 'trading_signals:add', payload: 1})
+  bus.emit({type: 'trading_signals:remove', payload: 1})
+  bus.emit({type: 'trading_signals:remove', payload: 1})
+
+
+
+  expect(subscriber).toHaveBeenCalledTimes(3)
+})
+
+test('with historySettings receives data from past on subscribe to \'select()\'', done => {
+  const bus = getInstance(new Map([
+    ['trading_signals:remove', 1]
+  ]))
+
+  const event = {type: 'trading_signals:remove', payload: 1}
+
+  bus.emit({type: 'trading_signals:remove', payload: 1})
+
+  bus
+    .select('trading_signals:remove')
+    .subscribe(e => {
+      expect(e).toEqual(event)
+      done()
+    })
+})
+
+
+
+
+
+
+
+
